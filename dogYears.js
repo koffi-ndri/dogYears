160cf7cfd6f01762eb060b7b5d47211d1a691603
Andrew N'dri <koffi.ndri@amalitech.org>
2021-08-24 13:08:38 +0000

    first commit

---
+++ dogYears.js
@@ -0,0 +1,8 @@
+const myAge = 27; //this is my age
+let earlyYears = 2; // this variable corresponds to the first two years of a dog's life
+earlyYears*=10.5;
+let laterYears = myAge - 2; // laterYears variable
+laterYears*=4; // The number of dog years
+let myAgeInDogYears = earlyYears + laterYears; // my age in dog years
+myName = 'Andrew'.toLowerCase(); // this store my name in lower case letters
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
